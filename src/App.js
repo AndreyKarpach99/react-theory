232b19c9c8ad852a7e11bb6303811be9edb5ebc8
Andreykas01 <andreykarpach99@gmail.com>
2021-02-26 12:18:21 +0300

    update 26.02.2021 12-18

--- src/App.js
+++ src/App.js
@@ -1,107 +1,63 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import './App.scss'
-import Car from './Car/Car'
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
-import Counter from './Counter/Counter'
-
-export const ClickedContext = React.createContext(false)
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom'
+import About from './About/About'
+import Cars from './Cars/Cars'
+import CarDetail from './CarDetail/CarDetail'
 
 class App extends Component {
 
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      clicked: false,
-      cars: [
-        {name: 'Ford', year: 2018},
-        {name: 'Audi', year: 2016},
-        {name: 'Mazda', year: 2010}
-      ],
-      pageTitle: 'React components',
-      showCars: false
-    }
-  }
-
-  toggleCarsHandler = () => {
-    this.setState({
-      showCars: !this.state.showCars
-    })
-  }
-
-  onChangeName(name, index) {
-    const car = this.state.cars[index]
-    car.name = name
-    const cars = [...this.state.cars]
-    cars[index] = car
-    this.setState({cars})
-  }
-
-  deleteHandler(index) {
-    const cars = this.state.cars.concat()
-    cars.splice(index, 1)
-
-    this.setState({cars})
-
-  }
-
-  componentWillMount() {
-    console.log('App componentWillMount')
-  }
-
-  componentDidMount() {
-    console.log('App componentDidMount')
+  state = {
+    isLoggedIn: false
   }
 
   render() {
-    console.log('App render')
-    const divStyle = {
-      textAlign: 'center'
-    }
-
-    let cars = null
-
-    if (this.state.showCars) {
-      cars = this.state.cars.map((car, index) => {
-        return (
-          <ErrorBoundary key={index}>
-            <Car
-              name={car.name}
-              year={car.year}
-              index={index}
-              onDelete={this.deleteHandler.bind(this, index)}
-              onChangeName={event => this.onChangeName(event.target.value, index)}
-            />
-          </ErrorBoundary>
-        )
-      })
-    }
 
     return (
-      <div style={divStyle}>
-        {/*<h1>{this.state.pageTitle}</h1>*/}
-        <h1>{this.props.title}</h1>
-
-        <ClickedContext.Provider value={this.state.clicked}>
-          <Counter />
-        </ClickedContext.Provider>
+      <div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <NavLink 
+                to="/"
+                exact
+                activeClassName={'wfm-active'}
+              >Home</NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to="/about"
+                activeStyle={{
+                  color: 'blue'
+                }}
+              >About</NavLink>
+            </li>
+            <li>
+              <NavLink to={{
+                pathname: '/cars'
+              }}>Cars</NavLink>
+            </li>
+          </ul>
+        </nav>
 
         <hr/>
-        <button
-          style={{marginTop: 20}}
-          className={'AppButton'}
-          onClick={this.toggleCarsHandler}
-        >Toggle cars</button>
 
-        <button onClick={() => this.setState({clicked: true})}>Change clicked</button>
-
-        <div style={{
-          width: 400,
-          margin: 'auto',
-          paddingTop: '20px'
-        }}>
-          { cars }
+        <div style={{textAlign: 'center'}}>
+          <h3>Is logged in {this.state.isLoggedIn ? 'TRUE' : 'FALSE'}</h3>
+          <button onClick={() => this.setState({isLoggedIn: true})}>Log in</button>
         </div>
+
+        <hr/>
+
+        <Switch>
+          <Route path="/" exact render={() => <h1>Home page</h1>} />
+          { this.state.isLoggedIn ? <Route path="/about" component={About} /> : null }
+          <Route path="/cars/:name" component={CarDetail} />
+          <Route path="/cars" component={Cars} />
+          <Redirect to={'/'} />
+          {/* <Route render={() => <h1 style={{color: 'red', textAlign: 'center'}}>404 Not found</h1>}/> */}
+        </Switch>
+
       </div>
     );
   }
